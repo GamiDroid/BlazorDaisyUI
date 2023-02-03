1a3db1b1f42a10ca946979dc0467efb32af8013b
Kevin Damen <k.damen@bellt.nl>
2023-02-04 00:46:57 +0100

    Save changes

--- BlazorDaisyUI/tailwind.config.js
+++ BlazorDaisyUI/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.{html,js,razor,razor.cs}"],
-  theme: {
+    content: ["**/*.{html,js,razor,razor.cs}"],
+    theme: {
     extend: {},
-  },
-  plugins: [],
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        prefix: "dsy-",
+    },
 }
